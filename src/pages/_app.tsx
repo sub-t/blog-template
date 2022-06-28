78f0e0fe92beaf86b323a29873104f4d56067c6e
sub-t <subt@outlook.jp>
2022-06-28 16:01:07 +0900

    [fix] authorの削除、他

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { DefaultSeo } from 'next-seo';
 import { defaultSeo } from 'next-seo.config';
 import { AppProps } from 'next/app';
 import { useRoutePath } from '@/lib/routePath';
-import '../styles/index.css';
+import '@/styles/index.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const path = useRoutePath();
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,15 +1,14 @@
-import { useRouter } from 'next/router';
 import ErrorPage from 'next/error';
-import Container from '../../components/container';
-import PostBody from '../../components/post-body';
-import PostHeader from '../../components/post-header';
-import Layout from '../../components/layout';
-import { getPostBySlug, getAllPosts } from '../../lib/api';
-import PostTitle from '../../components/post-title';
 import Head from 'next/head';
-import { CMS_NAME } from '../../lib/constants';
-import markdownToHtml from '../../lib/markdownToHtml';
-import PostType from '../../types/post';
+import { useRouter } from 'next/router';
+import Container from '@/components/container';
+import Layout from '@/components/layout';
+import PostBody from '@/components/post-body';
+import PostHeader from '@/components/post-header';
+import PostTitle from '@/components/post-title';
+import { getPostBySlug, getAllPosts } from '@/lib/api';
+import markdownToHtml from '@/lib/markdownToHtml';
+import type { PostType } from '@/types/post';
 
 type Props = {
   post: PostType;
@@ -31,16 +30,13 @@ const Post = ({ post, morePosts, preview }: Props) => {
           <>
             <article className="mb-32">
               <Head>
-                <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
-                </title>
+                <title>{post.title}</title>
                 <meta property="og:image" content={post.ogImage.url} />
               </Head>
               <PostHeader
                 title={post.title}
                 coverImage={post.coverImage}
                 date={post.date}
-                author={post.author}
               />
               <PostBody content={post.content} />
             </article>
