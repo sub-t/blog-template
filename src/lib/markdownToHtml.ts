8a13219e8fa186d3331f562ae10a25fb27c431c3
sub-t <subt@outlook.jp>
2022-07-02 06:30:40 +0900

    [update] remark

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,15 @@
 import { remark } from 'remark';
+import externalLinks from 'remark-external-links';
+import gfm from 'remark-gfm';
 import html from 'remark-html';
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
+  const result = await remark()
+    .use(html, { sanitize: false })
+    .use(gfm)
+    .use(externalLinks)
+    .use(require('remark-prism'))
+    .process(markdown);
+    
   return result.toString();
 }
