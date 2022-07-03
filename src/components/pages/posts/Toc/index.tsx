e702db371d53106b787c2f76851f18c6683f0e54
sub-t <subt@outlook.jp>
2022-07-03 14:02:53 +0900

    [update] 目次のスタイリング

--- src/components/pages/posts/Toc/index.tsx
+++ src/components/pages/posts/Toc/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { MdOutlineContentCopy } from 'react-icons/md';
 import tocbot from 'tocbot';
 
 export const Toc: React.VFC = () => {
@@ -14,7 +15,15 @@ export const Toc: React.VFC = () => {
 
   return (
     <div className="relative h-full">
-      <nav className="toc sticky top-1/2 -translate-y-1/2" />
+      <div className="lg:sticky lg:top-1/2 lg:-translate-y-1/2 flex flex-col gap-3 p-6">
+        <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center gap-2 py-2 px-3 border-b-2 border-indigo-700 dark:border-indigo-400 text-base font-bold text-base-style">
+            <MdOutlineContentCopy />
+            目次
+          </div>
+        </div>
+        <nav className="toc" />
+      </div>
     </div>
   );
 };
