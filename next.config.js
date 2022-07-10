2db12298d7dfe053e77cee182e6a4c4a231c8de9
sub-t <subt@outlook.jp>
2022-07-10 10:58:17 +0900

    [update] pagesにデプロイする準備

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: process.env.GITHUB_ACTIONS ? "/blog-template" : "",
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
