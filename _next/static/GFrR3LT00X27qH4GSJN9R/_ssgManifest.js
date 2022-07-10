6a34bd861f35829ed1f76c949e1a89d2d0b3f009
sub-t <sub-t@users.noreply.github.com>
2022-07-10 02:02:18 +0000

    deploy: b3f31ea8e10b92cc522640e0e779d6c310695818

---
+++ _next/static/GFrR3LT00X27qH4GSJN9R/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Ftags","\u002Fposts\u002F[slug]","\u002Fposts\u002Fpage\u002F[page]","\u002Ftags\u002F[tag]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
