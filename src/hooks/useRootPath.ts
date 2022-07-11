eca40f89a6f36918076e22f32975762ea8e4b0d9
sub-t <subt@outlook.jp>
2022-07-11 12:50:53 +0900

    [test] baseURLの変更

--- src/hooks/useRootPath.ts
+++ src/hooks/useRootPath.ts
@@ -1,9 +1,1 @@
-import { useRouter } from 'next/router';
-
-export const useRootPath = () => {
-  const router = useRouter();
-  const depth = router.pathname.split('/').filter(Boolean).length;
-  const cd = depth ? new Array<string>(depth).fill('..').join('/') : '.';
-
-  return cd;
-};
+export const useRootPath = () => '';
