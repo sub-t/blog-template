81d3f7535ad46a6f7ff7f3974ccaa5c856a38ab5
sub-t <98672681+sub-t@users.noreply.github.com>
2022-08-06 11:08:21 +0900

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -15,7 +15,7 @@ export const getMaxPage = () => {
 
 export const getPostBySlug = (slug: string, fields: string[] = []) => {
   const realSlug = slug.replace(/\.md$/, '');
-  const fullPath = join(postsDirectory, `${realSlug}.md`);
+  const fullPath = resolve(postsDirectory, `${realSlug}.md`);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
   const { data, content } = matter(fileContents);
 
